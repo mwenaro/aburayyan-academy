4eb86445921594a5d6334a2eaa62a11efdb8f89e
= <=>
2025-02-04 12:53:34 +0300

    chore: add home page pics

--- src/components/ict/Header.tsx
+++ src/components/ict/Header.tsx
@@ -3,7 +3,7 @@ import { Navbar } from "./Navbar";
 export const Header: React.FC = () => (
   <>
     <Navbar>
-      <h2 className="text-2xl md:text-3xl lg:text-4xl font-extrabold">
+      <h2 className="text-2xl md:text-3xl lg:text-4xl font-extrabold z-50">
         Abu-Rayyan Academy
       </h2>
     </Navbar>
