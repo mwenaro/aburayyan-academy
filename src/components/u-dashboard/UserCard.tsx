48e2a7d769e485a633d6f6b4db2ab38da4eae70e
= <=>
2025-02-26 16:42:40 +0300

    chore:add data files

--- src/components/u-dashboard/UserCard.tsx
+++ src/components/u-dashboard/UserCard.tsx
@@ -1,6 +1,9 @@
 import Image from "next/image";
-
-const UserCard = ({ type }: { type: string }) => {
+interface UserCardProps {
+  type:string 
+  num ?: number
+}
+const UserCard:React.FC<UserCardProps> = ({ type, num =1224 }) => {
   return (
     <div className="rounded-2xl odd:bg-lamaPurple even:bg-lamaYellow p-4 flex-1 min-w-[130px]">
       <div className="flex justify-between items-center">
@@ -9,8 +12,8 @@ const UserCard = ({ type }: { type: string }) => {
         </span>
         <Image src="/u-dashboard/more.png" alt="" width={20} height={20} />
       </div>
-      <h1 className="text-2xl font-semibold my-4">1,234</h1>
-      <h2 className="capitalize text-sm font-medium text-gray-500">{type}s</h2>
+      <h3 className="text-2xl font-semibold my-4">{num}</h3>
+      <h4 className="capitalize text-sm font-medium text-gray-500">{type}s</h4>
     </div>
   );
 };
