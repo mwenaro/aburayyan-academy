2034d1cae350ea3a89064635cb1c0613512937e8
Mwero Abdalla <mwerothewebmaker@gmail.com>
2025-03-28 14:38:31 +0300

    chore: add stduents to dashbaord

--- src/app/(pages)/u-dashboard/list/students/page.tsx
+++ src/app/(pages)/u-dashboard/list/students/page.tsx
@@ -1,90 +1,59 @@
+"use client";
+
+import { useEffect, useState } from "react";
 import FormModal from "@/components/u-dashboard/FormModal";
 import Pagination from "@/components/u-dashboard/Pagination";
 import Table from "@/components/u-dashboard/Table";
 import TableSearch from "@/components/u-dashboard/TableSearch";
-import { role, studentsData } from "@/lib/data";
 import Image from "next/image";
 import Link from "next/link";
-
-type Student = {
-  id: number;
-  studentId: string;
-  name: string;
-  email?: string;
-  photo: string;
-  phone?: string;
-  grade: number;
-  class: string;
-  address: string;
-};
+import { useStudentStore } from "@/lib/stores/studentStore";
+import { Button } from "@/components/ui/button";
 
 const columns = [
-  {
-    header: "Info",
-    accessor: "info",
-  },
-  {
-    header: "Student ID",
-    accessor: "studentId",
-    className: "hidden md:table-cell",
-  },
-  {
-    header: "Grade",
-    accessor: "grade",
-    className: "hidden md:table-cell",
-  },
-  {
-    header: "Phone",
-    accessor: "phone",
-    className: "hidden lg:table-cell",
-  },
-  {
-    header: "Address",
-    accessor: "address",
-    className: "hidden lg:table-cell",
-  },
-  {
-    header: "Actions",
-    accessor: "action",
-  },
+  { header: "Info", accessor: "info" },
+  { header: "Student ID", accessor: "studentId", className: "hidden md:table-cell" },
+  { header: "Grade", accessor: "grade", className: "hidden md:table-cell" },
+  { header: "Phone", accessor: "phone", className: "hidden lg:table-cell" },
+  { header: "Address", accessor: "address", className: "hidden lg:table-cell" },
+  { header: "Actions", accessor: "action" },
 ];
 
 const StudentListPage = () => {
-  const renderRow = (item: Student) => (
-    <tr
-      key={item.id}
-      className="border-b border-gray-200 even:bg-slate-50 text-sm hover:bg-lamaPurpleLight"
-    >
+const {students, fetchStudents} = useStudentStore()
+
+  useEffect(() => {
+    fetchStudents()
+     
+  }, []);
+
+  const renderRow = (item:any) => (
+    <tr key={item._id} className="border-b border-gray-200 even:bg-slate-50 text-sm hover:bg-lamaPurpleLight">
       <td className="flex items-center gap-4 p-4">
         <Image
-          src={item.photo}
-          alt=""
+          src={item.photo || "/default-avatar.png"}
+          alt="Student Photo"
           width={40}
           height={40}
           className="md:hidden xl:block w-10 h-10 rounded-full object-cover"
         />
         <div className="flex flex-col">
           <h3 className="font-semibold">{item.name}</h3>
-          <p className="text-xs text-gray-500">{item.class}</p>
+          <p className="text-xs text-gray-500">{item.class.name}</p>
         </div>
       </td>
-      <td className="hidden md:table-cell">{item.studentId}</td>
-      <td className="hidden md:table-cell">{item.grade}</td>
-      <td className="hidden md:table-cell">{item.phone}</td>
-      <td className="hidden md:table-cell">{item.address}</td>
+      <td className="hidden md:table-cell">{item._id}</td>
+      <td className="hidden md:table-cell">{item.class.grade}</td>
+      <td className="hidden md:table-cell">{item.contactDetails.phone}</td>
+      <td className="hidden md:table-cell">{item.address.town}, {item.address.county}</td>
       <td>
         <div className="flex items-center gap-2">
-          <Link href={`/list/teachers/${item.id}`}>
-            <button className="w-7 h-7 flex items-center justify-center rounded-full bg-lamaSky">
-              <Image src="/view.png" alt="" width={16} height={16} />
-            </button>
+          <Link href={`/list/students/${item._id}`}>
+            <Button className="w-7 h-7 flex items-center justify-center rounded-full bg-lamaSky">
+              <Image src="/view.png" alt="View" width={16} height={16} />
+            </Button>
           </Link>
-          {role === "admin" && (
-            // <button className="w-7 h-7 flex items-center justify-center rounded-full bg-lamaPurple">
-            //   <Image src="/delete.png" alt="" width={16} height={16} />
-            // </button>
-            <FormModal table="student" type="delete" id={item.id}/>
-          )}
+          <FormModal table="student" type="delete" id={item._id} />
         </div>
       </td>
     </tr>
@@ -92,30 +61,22 @@ const StudentListPage = () => {
 
   return (
     <div className="bg-white p-4 rounded-md flex-1 m-4 mt-0">
-      {/* TOP */}
       <div className="flex items-center justify-between">
         <h1 className="hidden md:block text-lg font-semibold">All Students</h1>
         <div className="flex flex-col md:flex-row items-center gap-4 w-full md:w-auto">
           <TableSearch />
           <div className="flex items-center gap-4 self-end">
-            <button className="w-8 h-8 flex items-center justify-center rounded-full bg-lamaYellow">
-              <Image src="/u-dashboard/filter.png" alt="" width={14} height={14} />
-            </button>
-            <button className="w-8 h-8 flex items-center justify-center rounded-full bg-lamaYellow">
-              <Image src="/u-dashboard/sort.png" alt="" width={14} height={14} />
-            </button>
-            {role === "admin" && (
-              // <button className="w-8 h-8 flex items-center justify-center rounded-full bg-lamaYellow">
-              //   <Image src="/plus.png" alt="" width={14} height={14} />
-              // </button>
-              <FormModal table="student" type="create"/>
-            )}
+            <Button className="w-8 h-8 flex items-center justify-center rounded-full bg-lamaYellow">
+              <Image src="/u-dashboard/filter.png" alt="Filter" width={14} height={14} />
+            </Button>
+            <Button className="w-8 h-8 flex items-center justify-center rounded-full bg-lamaYellow">
+              <Image src="/u-dashboard/sort.png" alt="Sort" width={14} height={14} />
+            </Button>
+            <FormModal table="student" type="create" />
           </div>
         </div>
       </div>
-      {/* LIST */}
-      <Table columns={columns} renderRow={renderRow} data={studentsData} />
-      {/* PAGINATION */}
+      <Table columns={columns} renderRow={renderRow} data={students} />
       <Pagination />
     </div>
   );
